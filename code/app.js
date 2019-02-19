f0c6834bfe63402a747e86525ab53cc0c578b4f8
Olteanu Cosmin <43538300+olteCo@users.noreply.github.com>
2019-02-19 20:55:11 +0100

    dynamic content and add button add

--- code/app.js
+++ code/app.js
@@ -1,14 +1,56 @@
-var app = {
 
+var app = {
     init: function () {
-        // alert("Benvenuti nel Bar-Ista ");
-        $.get("https://api.github.com/users/olteCo")
-            .done(app.onSuccess);
+        console.log("init inside app!");
+        $("title").text("Bar-Ista");
+        $.getJSON("data.json")
+            .done(app.onSuccess)
+            .fail(app.onError);
+
     },
-    onSuccess: function (data) {
-        console.log(data)
+    onSuccess: function (jsonData) {
+        console.log(jsonData);
+        jsonData.productList.forEach(element => {
+            let productRow = `
+            <div class="product">
+            <div class="image_product">
+                    <img src="/img/${element.product}.png" alt="">
+                </div>
+                <div class="like">
+                            <i class="far fa-heart" ></i>
+                            <p><span id="count">${element.like}</span></p>
+                </div>
+                <div class="descrition">
+                        <p class="nome">${element.product}</p>
+                        <p class="prezzo">${element.price}euro</p>
+                </div>
+                <div class="add">
+                    <button class="btnAdd">Add</button>
+                   
+                </div>
+            </div>`;
+            $("#main").append(productRow);
+        });
+
+    },
+
+    onError: function (e) {
+        console.log("error!");
+        console.log(JSON.stringify(e));
     }
 };
 
+$(document).ready(app.init);/*
+document.getElementsByClassName("far fa-heart")[0].ondblclick=like;
+document.getElementsByClassName("btnAdd")[0].onclick=addToShop;
+function addToShop(){
+   console.log("Click me");
 
-$(document).ready(app.init);
+
+}
+function like(){
+    var like=document.getElementById("count");
+    document.getElementById("count").innerHTML=100+1;
+    document.getElementsByClassName("far fa-heart")[0].style.backgroundColor="red";
+}
+*/
