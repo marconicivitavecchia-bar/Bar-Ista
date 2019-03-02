574e18cdb73c62dd5f8263b1acbc425f4a2f3f8e
Olteanu Cosmin <43538300+olteCo@users.noreply.github.com>
2019-03-02 11:22:41 +0100

    update data.php

--- code/data.php
+++ code/data.php
@@ -1,19 +1,19 @@
 <?php
 // Creo un nuovo oggetto
 $menuObj = new stdClass();
-$menuObj->productList = array(new stdClass(),new stdClass());
+$menuObj->productList = array(new stdClass(),new stdClass(),new stdClass(),new stdClass());
 $menuObj->productList[0]->product = "Pizza bianca";
 $menuObj->productList[0]->price = 1;
-$menuObj->productList[0]->like = 0;
-$menuObj->productList[1]->product = "caffè";
-$menuObj->productList[1]->price = 0.60;
-$menuObj->productList[1]->like = 273;
-$menuObj->productList[2]->product = "Hot Dog";
-$menuObj->productList[2]->price = 2;
-$menuObj->productList[2]->like = 129;
-$menuObj->productList[3]->product = "Pizza rossa";
-$menuObj->productList[3]->price = 1,20;
-$menuObj->productList[3]->like = 95;
+$menuObj->productList[0]->like = 32;
+$menuObj->productList[1]->product = "Pizza rossa";
+$menuObj->productList[1]->price = 1.20;
+$menuObj->productList[1]->like = 284;
+$menuObj->productList[2]->product = "Caffè";
+$menuObj->productList[2]->price = 0.600;
+$menuObj->productList[2]->like = 375;
+$menuObj->productList[3]->product = "Hot dog";
+$menuObj->productList[3]->price = 2;
+$menuObj->productList[3]->like = 184;
 $menuJson = json_encode($menuObj);
 echo $menuJson;
 ?>
